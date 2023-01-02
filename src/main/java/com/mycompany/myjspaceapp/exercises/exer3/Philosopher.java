8ac213f450b33b3e7145d5f8a4e4c5d944996223
Zahed <zahedm45@gmail.com>
2023-01-02 20:56:06 +0100

    Cleaned up

--- src/main/java/com/mycompany/myjspaceapp/exercises/exer3/Philosopher.java
+++ src/main/java/com/mycompany/myjspaceapp/exercises/exer3/Philosopher.java
@@ -1,48 +1,19 @@
 package com.mycompany.myjspaceapp.exercises.exer3;
 
-
-import com.mycompany.myjspaceapp.exercises.exer2.philosopher.Exercise_2_1;
 import org.jspace.ActualField;
-import org.jspace.RandomSpace;
 import org.jspace.RemoteSpace;
 import org.jspace.Space;
-
 import java.io.IOException;
 
 class Philosopher {
+    static int numPhilosophers = 0;
+    static int me = 0;
+    static int port = 31145;
+    static String host = "localhost";
+
     public static void main(String[] args) {
-        int numPhilosophers = 0;
-        int me = 0;
-        int port = 31145;
-        String host = "localhost";
 
-        if (args.length < 2 || args.length > 4) {
-            System.out.println("Wrong number of arguments");
-            System.out.println("Usage: java -jar run main.jar <number of philosophers> <my id> [host] [port]");
-            return;
-        }
-
-        numPhilosophers = Integer.parseInt(args[0]);
-
-        if (numPhilosophers <= 1) {
-            System.out.println("Wrong number of philosophers. Must be at least 2.");
-            return;
-        }
-
-        me = Integer.parseInt(args[1]);
-        if (me < 0 || me >= numPhilosophers) {
-            System.out.println("Wrong philosopher id. Must be between 0 and " + (numPhilosophers - 1));
-            return;
-        }
-        if (args.length >= 3) {
-            host = args[2];
-        }
-        if (args.length >= 4) {
-            port = Integer.parseInt(args[3]);
-        }
-
-
-
+        initialize(args);
 
         try {
             String uri = "tcp://" + host + ":" + port + "/board?conn";
@@ -77,28 +48,8 @@ class Philosopher {
                 // Return the forks (put the corresponding tuples).
                 board.put("fork", left);
                 board.put("fork", right);
-                board.put("lock");
-                System.out.println("Philosopher " + me + " put both forks and a ticket on the table");
-
-
-/*                board.get(new ActualField("lock"));
-
-                // Wait until the left fork is ready (get the corresponding tuple).
-                board.get(new ActualField("fork"), new ActualField(left));
-                System.out.println("Philosopher " + me + " got left fork");
-
-                // Wait until the right fork is ready (get the corresponding tuple).
-                board.get(new ActualField("fork"), new ActualField(right));
-                System.out.println("Philosopher " + me + " got right fork");
-
-                // Lunch time.
-                System.out.println("Philosopher " + me + " is eating...");
-
-                // Return the forks (put the corresponding tuples).
-                board.put("fork", left);
-                board.put("fork", right);
                 System.out.println("Philosopher " + me + " put both forks on the table");
-                board.put("lock");*/
+                board.put("lock");
 
             } catch (InterruptedException e) {}
 
@@ -107,4 +58,30 @@ class Philosopher {
     }
 
 
+    static void initialize(String[] args) {
+        if (args.length < 2 || args.length > 4) {
+            System.out.println("Wrong number of arguments");
+            System.out.println("Usage: java -jar run main.jar <number of philosophers> <my id> [host] [port]");
+            return;
+        }
+
+        numPhilosophers = Integer.parseInt(args[0]);
+
+        if (numPhilosophers <= 1) {
+            System.out.println("Wrong number of philosophers. Must be at least 2.");
+            return;
+        }
+
+        me = Integer.parseInt(args[1]);
+        if (me < 0 || me >= numPhilosophers) {
+            System.out.println("Wrong philosopher id. Must be between 0 and " + (numPhilosophers - 1));
+            return;
+        }
+        if (args.length >= 3) {
+            host = args[2];
+        }
+        if (args.length >= 4) {
+            port = Integer.parseInt(args[3]);
+        }
+    }
 }
